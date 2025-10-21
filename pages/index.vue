2b9ca7be2b784e6b81c515342f23673ef9522034
Conner Parsley <conner.c.wells@gmail.com>
2025-10-21 10:50:34 -0700

    initial pages setup

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>I love zen gardens</div>
+</template>
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/svg",
+      href: "/favicon.svg",
+    },
+  ],
+});
+</script>
---
+++ pages/work/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>I am a work landing page!</div>
+</template>
